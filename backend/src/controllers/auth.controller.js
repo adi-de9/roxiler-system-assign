9dd2c24648f2344e053189638447c54025973e25
Aditya Deshmukh <143163173+adi-de9@users.noreply.github.com>
2025-12-04 13:57:31 +0530

    Change cookie options to secure in auth controller
    Updated cookie options to secure for production.

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
-export const options = { httpOnly: true, secure: false,sameSite: "none" };
+export const options = { httpOnly: true, secure: true,sameSite: "none" };
 
 export const signup = async (req, res) => {
   const { name, email, password, address } = req.body;
