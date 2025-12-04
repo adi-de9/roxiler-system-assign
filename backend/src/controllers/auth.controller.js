3e7b3e182d1901557eb9d23ede290078c5b59d1b
Aditya Deshmukh <143163173+adi-de9@users.noreply.github.com>
2025-12-04 13:52:58 +0530

    Add sameSite option to cookie settings

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -2,7 +2,7 @@ import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import { User } from "../models/user.model.js";
 
-export const options = { httpOnly: true, secure: false };
+export const options = { httpOnly: true, secure: false,sameSite: "none" };
 
 export const signup = async (req, res) => {
   const { name, email, password, address } = req.body;
